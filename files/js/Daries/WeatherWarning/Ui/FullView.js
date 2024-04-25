f20d4232342552eff33d7bcbdec63ee6ea8689e9
xDeraiser <deraiser@gmx.de>
2024-04-25 05:42:09 +0200

    add fullView

---
+++ files/js/Daries/WeatherWarning/Ui/FullView.js
@@ -0,0 +1,22 @@
+/**
+ * @author  Marco Daries, Alexander Langer (Source of ideas)
+ * @copyright   2020-2024 Daries.dev
+ * @license Daries.dev - Free License <https://daries.dev/license-for-free-plugins>
+ */
+define(["require", "exports", "WoltLabSuite/Core/Component/Dialog", "WoltLabSuite/Core/Helper/Selector"], function (require, exports, Dialog_1, Selector_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.setup = void 0;
+    function setup() {
+        (0, Selector_1.wheneverFirstSeen)("[data-weather-info-full-view]", (element) => {
+            element.addEventListener("click", () => {
+                showDialog(element);
+            });
+        });
+    }
+    exports.setup = setup;
+    function showDialog(element) {
+        const dialog = (0, Dialog_1.dialogFactory)().fromHtml(element.innerHTML).withoutControls();
+        dialog.show(element.dataset.weatherInfoFullView);
+    }
+});
---
+++ ts/Daries/WeatherWarning/Ui/FullView.ts
@@ -0,0 +1,21 @@
+/**
+ * @author  Marco Daries, Alexander Langer (Source of ideas)
+ * @copyright   2020-2024 Daries.dev
+ * @license Daries.dev - Free License <https://daries.dev/license-for-free-plugins>
+ */
+
+import { dialogFactory } from "WoltLabSuite/Core/Component/Dialog";
+import { wheneverFirstSeen } from "WoltLabSuite/Core/Helper/Selector";
+
+export function setup(): void {
+  wheneverFirstSeen("[data-weather-info-full-view]", (element: HTMLElement) => {
+    element.addEventListener("click", () => {
+      showDialog(element);
+    });
+  });
+}
+
+function showDialog(element: HTMLElement): void {
+  const dialog = dialogFactory().fromHtml(element.innerHTML).withoutControls();
+  dialog.show(element.dataset.weatherInfoFullView!);
+}
